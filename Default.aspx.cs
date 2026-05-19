dce6155fd62db6aac5442ec74a66a558612bc94b
dhanasyamala <sdhanavenkat@gmail.com>
2026-05-20 02:04:38 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sriramaproject
+{
+	public partial class _Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			txtName.Text = "How are you?";
+		}
+	}
+}
